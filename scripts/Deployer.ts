8b97b289d1b48db82ca1f12ee2069112134a9350
0xAtum <0xAtum@protonmail.com>
2022-02-03 22:03:49 -0500

    Final Version

--- scripts/Deployer.ts
+++ scripts/Deployer.ts
@@ -2,11 +2,15 @@ import { IDeployConfig } from "./config/DeployConfig"
 import { DeploymentHelper } from "./utils/DeploymentHelper"
 import { ethers } from "hardhat"
 import BalanceTree from "./misc/balance-tree"
-import { BigNumber } from "ethers"
+import { BigNumber, Contract, Signer } from "ethers"
+import vstaAbi from "./abis/VSTAToken.json"
 
 export class Deployer {
 	config: IDeployConfig
 	helper: DeploymentHelper
+	deployerWallet?: Signer
+	tokenPrice: BigNumber = BigNumber.from("375000000000000000") //0.375
+	e6_to_e18 = BigNumber.from(10).pow(12)
 
 	constructor(config: IDeployConfig) {
 		this.config = config
@@ -14,51 +18,85 @@ export class Deployer {
 	}
 
 	async run() {
-		const tokenPrice: BigNumber = BigNumber.from("375000000000000000") //0.375
-		const e6_to_e18 = BigNumber.from(10).pow(12)
+		this.deployerWallet = (await ethers.getSigners())[0]
+
+		const [vsta, whitelisting, vesting] = await this.getContracts()
+
+		const allowance = await vsta.allowance(
+			this.deployerWallet.getAddress(),
+			whitelisting.address
+		)
+
+		if (allowance == 0) {
+			await vsta.approve(whitelisting.address, ethers.constants.MaxUint256)
+		}
+
+		if (!(await whitelisting.isInitialized)) {
+			await this.initWhitelisting(whitelisting, vesting)
+			console.log("Whitelisting Initalized")
+		}
+
+		if (!(await vesting.isInitialized)) {
+			await this.initVesting(vesting, whitelisting)
+			console.log("Vesting Initalized")
+		}
+
+		await whitelisting.transferOwnership(this.config.adminWallet)
+	}
+
+	async getContracts() {
+		const vsta = new ethers.Contract(
+			this.config.vestaTokenAddress,
+			vstaAbi.abi,
+			this.deployerWallet
+		)
 
 		const Whitelisting = await ethers.getContractFactory("Whitelisting")
-		const VestingVsta = await ethers.getContractFactory("VestingVsta")
+		const VestingVsta = await ethers.getContractFactory("WhitelistVestingVesta")
 		const whitelisting = await this.helper.deployContract(
 			Whitelisting,
 			"Whitelisting"
 		)
 
-		const vesting = await this.helper.deployContract(VestingVsta, "VestingVsta")
+		const vesting = await this.helper.deployContract(
+			VestingVsta,
+			"WhitelistVestingVesta"
+		)
 
-		if (!(await whitelisting.isInitialized)) {
-			const bt = new BalanceTree(this.config.whitelist)
-			let totalAmountUSDC = BigNumber.from("0")
+		return [vsta, whitelisting, vesting]
+	}
 
-			this.config.whitelist.forEach(wl => {
-				totalAmountUSDC = totalAmountUSDC.add(wl.amount)
-			})
+	async initWhitelisting(whitelisting: Contract, vesting: Contract) {
+		const bt = new BalanceTree(this.config.whitelist)
+		let totalAmountUSDC = BigNumber.from("0")
 
-			//TO ETHER
-			totalAmountUSDC = totalAmountUSDC.mul(e6_to_e18)
-			const totalAmountToken = totalAmountUSDC
-				.div(tokenPrice)
-				.mul(ethers.utils.parseEther("1"))
+		this.config.whitelist.forEach(wl => {
+			totalAmountUSDC = totalAmountUSDC.add(wl.amount)
+		})
 
-			console.log(totalAmountToken)
-			console.log("Root:", bt.getHexRoot())
+		//TO ETHER
+		totalAmountUSDC = totalAmountUSDC.mul(this.e6_to_e18)
+		const totalAmountToken = totalAmountUSDC
+			.div(this.tokenPrice)
+			.mul(ethers.utils.parseEther("1"))
 
-			await whitelisting.setAddresses(
-				this.config.vestaTokenAddress,
-				vesting.address,
-				tokenPrice,
-				totalAmountToken.toString(),
-				bt.getHexRoot()
-			)
-		}
+		console.log(totalAmountToken)
+		console.log("Root:", bt.getHexRoot())
 
-		if (!(await vesting.isInitialized)) {
+		await whitelisting.setAddresses(
 			this.config.vestaTokenAddress,
-				await vesting.setAddresses(
-					this.config.vestaTokenAddress,
-					whitelisting.address
-				)
-		}
-		await whitelisting.transferOwnership(this.config.adminWallet)
+			vesting.address,
+			this.tokenPrice,
+			totalAmountToken.toString(),
+			bt.getHexRoot()
+		)
+	}
+
+	async initVesting(vesting: Contract, whitelisting: Contract) {
+		this.config.vestaTokenAddress,
+			await vesting.setAddresses(
+				this.config.vestaTokenAddress,
+				whitelisting.address
+			)
 	}
 }
--- scripts/deploy.testnet.ts
+++ scripts/deploy.testnet.ts
@@ -1,12 +1,12 @@
 import { IDeployConfig } from "./config/DeployConfig"
-import { DeploymentHelper, ZERO_ADDRESS } from "./utils/DeploymentHelper"
+import { DeploymentHelper } from "./utils/DeploymentHelper"
 import { Deployer } from "./Deployer"
 import { BigNumber } from "ethers"
 
 const config: IDeployConfig = {
 	outputFile: "./testnet_deployments.json",
 	adminWallet: "0x87209dc4B76b14B67BC5E5e5c0737E7d002a219c",
-	vestaTokenAddress: "",
+	vestaTokenAddress: "0x183CEe30F1C9c126907d8355f25cad762632D7aE",
 	totalSupply: 787787,
 	whitelist: [
 		{
@@ -21,20 +21,17 @@ const config: IDeployConfig = {
 			account: "0xCe24DB336Fa759d16f6B87A82D397d91EcBb3296",
 			amount: BigNumber.from("1484144112"),
 		},
-		{
-			account: "0x87209dc4B76b14B67BC5E5e5c0737E7d002a219c",
-			amount: BigNumber.from("641025641"),
-		},
 	],
 }
 
 async function main() {
 	const helper = new DeploymentHelper(config)
-	const ercMock = await helper.deployContractByName("ERC20Mock")
-	await ercMock.mint(config.adminWallet, 100_000e18)
 
-	config.vestaTokenAddress = ercMock.address
-	config.vestaTokenAddress = ZERO_ADDRESS
+	if (config.vestaTokenAddress === "") {
+		const ercMock = await helper.deployContractByName("ERC20Mock")
+		await ercMock.mint(config.adminWallet, 100_000e18)
+		config.vestaTokenAddress = ercMock.address
+	}
 
 	await new Deployer(config).run()
 }
